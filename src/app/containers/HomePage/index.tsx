7a4c6803464689702f8b67ed5189a7881a7c079e
Javier <52850238+Jaserto@users.noreply.github.com>
2021-08-07 08:05:34 +0200

    first commit

---
+++ src/app/containers/HomePage/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import tw from 'twin.macro';
+import { TopSection } from './topSection';
+import { Navbar } from '../../components/navbar';
+import { BookCard } from '../../components/bookCard';
+
+const PageContainer = styled.div`
+    ${tw`
+        flex
+        flex-col
+        w-full
+        h-full
+        items-center
+        overflow-x-hidden
+    `}
+`;
+
+export function HomePage() {
+    return (
+    <PageContainer>
+        <Navbar />
+        <TopSection />
+        <BookCard />
+    </PageContainer>
+    );
+}
